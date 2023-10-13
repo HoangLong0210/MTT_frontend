98a8867b26bdc8b599b883247631570291082765
HoangLong <pdhlong933@gmail.com>
2023-10-13 13:27:03 +0700

    .

--- src/reducers/Tags/tags.js
+++ src/reducers/Tags/tags.js
@@ -1,6 +1,6 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import { toastSuccess, toastError } from "../.././Toast/Toast"
-import axios from "axios"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toastSuccess, toastError } from "../.././Toast/Toast";
+import axios from "axios";
 // import setAuthToken from "../../utils/setAuthToken"
 // import * as types from "../.././contains/types"
 
@@ -8,30 +8,28 @@ export const loadTags = createAsyncThunk(
   "tags/fetchTags",
   async (id_account) => {
     try {
-      const response = await axios.get(
-        `http://localhost:8080/api/v2/tag/all`
-      )
+      const response = await axios.get(`http://localhost:8080/api/v2/tag/all`);
       if (response.status === 200) {
-        return await { ...response.data, status: response.status }
+        return await { ...response.data, status: response.status };
       }
     } catch (error) {
-      if (error.response.data) return error.response.data
-      else return { message: error.message }
+      if (error.response.data) return error.response.data;
+      else return { message: error.message };
     }
   }
-)
+);
 
 export const loadTagsAll = createAsyncThunk("tags/fetchTagAll", async () => {
   try {
-    const response = await axios.get(`http://localhost:8080/api/v2/tag/all`)
+    const response = await axios.get(`http://localhost:8080/api/v2/tag/all`);
     if (response.status === 200) {
-      return await { ...response.data, status: response.status }
+      return await { ...response.data, status: response.status };
     }
   } catch (error) {
-    if (error.response.data) return error.response.data
-    else return { message: error.message }
+    if (error.response.data) return error.response.data;
+    else return { message: error.message };
   }
-})
+});
 
 export const loadTagsPost = createAsyncThunk(
   "tags/fetchTagPost",
@@ -39,16 +37,16 @@ export const loadTagsPost = createAsyncThunk(
     try {
       const response = await axios.get(
         `http://localhost:8080/api/v2/tag/${id_tag}/posts`
-      )
+      );
       if (response.status === 200) {
-        return await { ...response.data, status: response.status }
+        return await { ...response.data, status: response.status };
       }
     } catch (error) {
-      if (error.response.data) return error.response.data
-      else return { message: error.message }
+      if (error.response.data) return error.response.data;
+      else return { message: error.message };
     }
   }
-)
+);
 
 export const addTag = createAsyncThunk("tags/addTag", async (tag) => {
   try {
@@ -57,52 +55,52 @@ export const addTag = createAsyncThunk("tags/addTag", async (tag) => {
         Accept: "application/json",
         "Content-Type": "multipart/form-data",
       },
-    })
+    });
     if (response.status === 201 || response.status === 200) {
-      return await { ...response.data, status: response.status }
+      return await { ...response.data, status: response.status };
     }
   } catch (error) {
-    if (error.response.data) return error.response.data
-    else return { message: error.message }
+    if (error.response.data) return error.response.data;
+    else return { message: error.message };
   }
-})
+});
 
 export const updateTagAll = createAsyncThunk(
   "tags/updateTagAll",
   async (tag) => {
-    const id_tag = tag.get("id_tag")
+    const id_tag = tag.get("id_tag");
     try {
       const response = await axios.put(
         `http://localhost:8080/api/v2/tag/${id_tag}`,
         tag
-      )
+      );
       if (response.status === 200) {
-        console.log(response.data)
-        return await { ...response.data, status: response.status }
+        console.log(response.data);
+        return await { ...response.data, status: response.status };
       }
     } catch (error) {
-      if (error.response.data) return error.response.data
-      else return { message: error.message }
+      if (error.response.data) return error.response.data;
+      else return { message: error.message };
     }
   }
-)
+);
 export const followTagALL = createAsyncThunk("tags/follow", async (id_tag) => {
   try {
     const response = await axios.post(
       `http://localhost:8080/api/v2/follow_tag/${id_tag}`
-    )
+    );
     if (response.status === 200) {
       return await {
         ...response.data,
         status: response.status,
         id_tag: id_tag,
-      }
+      };
     }
   } catch (error) {
-    if (error.response.data) return error.response.data
-    else return { message: error.message }
+    if (error.response.data) return error.response.data;
+    else return { message: error.message };
   }
-})
+});
 
 export const deleteFollowTagALL = createAsyncThunk(
   "tags/delete",
@@ -110,29 +108,29 @@ export const deleteFollowTagALL = createAsyncThunk(
     try {
       const response = await axios.delete(
         `http://localhost:8080/api/v2/follow_tag/${id_tag}`
-      )
+      );
       if (response.status === 200) {
-        return await { ...response.data, id_tag: id_tag }
+        return await { ...response.data, id_tag: id_tag };
       }
     } catch (error) {
-      if (error.response.data) return error.response.data
-      else return { message: error.message }
+      if (error.response.data) return error.response.data;
+      else return { message: error.message };
     }
   }
-)
+);
 export const findTagT = createAsyncThunk("tags/find", async (data) => {
   try {
     const response = await axios.get(
       `http://localhost:8080/api/v2/tag/${data.id_tag}`
-    )
+    );
     if (response.status === 200) {
-      return await { ...response.data, status: response.status }
+      return await { ...response.data, status: response.status };
     }
   } catch (error) {
-    if (error.response.data) return error.response.data
-    else return { message: error.message }
+    if (error.response.data) return error.response.data;
+    else return { message: error.message };
   }
-})
+});
 const tags = createSlice({
   name: "tags",
   initialState: {
@@ -146,24 +144,24 @@ const tags = createSlice({
     builder
       .addCase(loadTags.fulfilled, (state, action) => {
         if (action.payload.status === 200) {
-          state.tags = action.payload.data
+          state.tags = action.payload.data;
         } else {
-          toastError(action.payload.message)
+          toastError(action.payload.message);
         }
       })
       .addCase(loadTagsAll.fulfilled, (state, action) => {
         if (action.payload.status === 200) {
-          state.tagsAll = action.payload.data
+          state.tagsAll = action.payload.data;
         } else {
-          toastError(action.payload.message)
+          toastError(action.payload.message);
         }
       })
       .addCase(addTag.fulfilled, (state, action) => {
         if (action.payload.status === 201 || action.payload.status === 200) {
-          state.tagsAll.unshift(action.payload.data)
-          toastSuccess(action.payload.message)
+          state.tagsAll.unshift(action.payload.data);
+          toastSuccess(action.payload.message);
         } else {
-          toastError(action.payload.message)
+          toastError(action.payload.message);
         }
       })
       .addCase(updateTagAll.fulfilled, (state, action) => {
@@ -172,53 +170,53 @@ const tags = createSlice({
             tag.id_tag === action.payload.data.id_tag
               ? action.payload.data
               : tag
-          )
-          toastSuccess(action.payload.message)
+          );
+          toastSuccess(action.payload.message);
         } else {
-          toastError(action.payload.message)
+          toastError(action.payload.message);
         }
       })
       .addCase(followTagALL.fulfilled, (state, action) => {
-        state.tag.status = !state.tag.status
+        state.tag.status = !state.tag.status;
         if (action.payload.status === 200) {
           state.tags = state.tags.map((tag) => {
             if (tag.id_tag === action.payload.id_tag && tag.status === false)
-              tag.status = !tag.status
-            return tag
-          })
-          toastSuccess(action.payload.message)
+              tag.status = !tag.status;
+            return tag;
+          });
+          toastSuccess(action.payload.message);
         } else {
-          toastError(action.payload.message)
+          toastError(action.payload.message);
         }
       })
       .addCase(deleteFollowTagALL.fulfilled, (state, action) => {
-        state.tag.status = !state.tag.status
+        state.tag.status = !state.tag.status;
         if (action.payload.status === 200) {
           state.tags = state.tags.map((tag) => {
             if (tag.id_tag === action.payload.id_tag && tag.status === true)
-              tag.status = !tag.status
-            return tag
-          })
-          toastSuccess(action.payload.message)
+              tag.status = !tag.status;
+            return tag;
+          });
+          toastSuccess(action.payload.message);
         } else {
-          toastError(action.payload.message)
+          toastError(action.payload.message);
         }
       })
       .addCase(findTagT.fulfilled, (state, action) => {
-        state.tag = action.payload.data
+        state.tag = action.payload.data;
       })
       .addCase(loadTagsPost.fulfilled, (state, action) => {
-        state.tagPost = action.payload.data
-      })
+        state.tagPost = action.payload.data;
+      });
   },
-})
+});
 
-const tagReducer = tags.reducer
+const tagReducer = tags.reducer;
 
-export const tagsSelector = (state) => state.tagReducer.tags
-export const tagsAllSelector = (state) => state.tagReducer.tagsAll
-export const { findT } = tags.actions
-export const tagSelector = (state) => state.tagReducer.tag
-export const tagsPostSelector = (state) => state.tagReducer.tagPost
+export const tagsSelector = (state) => state.tagReducer.tags;
+export const tagsAllSelector = (state) => state.tagReducer.tagsAll;
+export const { findT } = tags.actions;
+export const tagSelector = (state) => state.tagReducer.tag;
+export const tagsPostSelector = (state) => state.tagReducer.tagPost;
 
-export default tagReducer
+export default tagReducer;
